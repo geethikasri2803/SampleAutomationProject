08110edd711803958ff3135249a6470bfddec437
geethikasri <honey0881@gmail.com>
2024-01-30 15:22:27 +0530

    newly added project

---
+++ src/main/java/SampleAutomation/SampleAutomation/App.java
@@ -0,0 +1,13 @@
+package SampleAutomation.SampleAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/sampletests/qa/TestClass.java
@@ -0,0 +1,22 @@
+package com.sampletests.qa;
+
+import org.testng.annotations.Test;
+
+public class TestClass{
+	@Test
+	public void SampleTestOne() {
+		System.out.println("test 1 is passed");
+		
+	}
+	@Test
+	public void SampleTestTwo() {
+		System.out.println("test 2 is passed");
+		
+	}
+	@Test
+	public void SampleTestThree() {
+		System.out.println("test 3 is passed");
+		
+	}
+
+}
